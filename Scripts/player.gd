0e561e2f4fe0347e58779d88d39ee55202bced49
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-08-27 12:03:10 +1000

    leason 1 tile map and polayer movment

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,9 +14,32 @@ func process_movment() -> void:
 	var direction := Input.get_vector("left","right","up","down")
 	
 	velocity = direction * SPEED
+	process_animation(direction)
 	
-func play_animation(dir: Vector2) -> void:
-	if dir.x > 0:
-		animated_sprite_2d.play()
-	 
+	#run or idle logic
+func process_animation(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_animation("Walk",direction)
+		#print(velocity)
+		#print_rich("Walk")
+	elif velocity == Vector2.ZERO:
+		play_animation("Idle",direction)
+		#print(velocity)
+		#print("Idle")
+	
+	#to play an animation based on direction
+func play_animation(prefix: String,dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix +"_Right")
+		
+	#elif dir.x < 0:
+	#	animated_sprite_2d.play(prefix +"_Left")
+		
+	elif dir.y >= 0:
+		animated_sprite_2d.play(prefix +"_Forward")
+		
+	elif dir.y <= 0:
+		animated_sprite_2d.play(prefix +"_Up")
+	
 	
