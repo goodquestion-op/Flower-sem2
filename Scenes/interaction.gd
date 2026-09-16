a7e3b65db5d7a68939eda6ff8793faecbe44a5c7
Ash Swainson <0348422@schoolsnet.act.edu.au>
2026-09-16 14:58:56 +1000

    adding more flower logic

--- Scenes/interaction.gd
+++ Scenes/interaction.gd
@@ -1,23 +1,24 @@
 extends Node2D
 class_name Harvester
 
+
 @onready var hitBox : Area2D = $Area2D
+var harvestable = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("harvest"):
+		if (harvestable == true):
+			print("harvesting")
+			pass
+			#queue_free() 
 		
-		print("harvesting")
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if(body is Flower):
-		print("leaving Flower")
-	elif(body is Flower_2):
-		print("Leaving Flower_2" )
+	harvestable = false
+	print("leaving:",body) 
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if(body is Flower):
-		print("entering Flower" )
-	elif(body is Flower_2):
-		print("entering Flower_2" )
+	harvestable = true
+	print("entering:",body) 
